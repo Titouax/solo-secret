4d66567148fa2d7b7bb2802496f3a29cd33c800e
Titouax <titouan.dessus@gmail.com>
2018-08-05 18:43:32 +0200

    CRUD et password digest fonctionnel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
-
-  before_save { self.email = email.downcase! }
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
